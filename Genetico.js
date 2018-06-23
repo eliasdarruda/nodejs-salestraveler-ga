31af262c444cb4b82562632a3807223f1d3e6601
Elias Arruda <eliasdarruda@gmail.com>
2018-06-23 00:23:04 -0300

    ops

--- Genetico.js
+++ Genetico.js
@@ -218,7 +218,7 @@ function initGenetic(map, areaEmPx, genConfig, sucess) {
     };
 
     genetic.notification = function (pop, generation, stats, isFinished) {
-        console.log(pop.entity[0]);
+        console.log(pop[0].entity);
         console.log('-------------');
         console.log(stats);
         console.log('-------------');
